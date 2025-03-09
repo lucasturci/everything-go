dda5a6f4e6f2727ff8b46b3fe81f6d33ef0f5b3b
Lucas Turci <lucas.turci@gmail.com>
2025-03-09 11:06:40 -0300

    [data-structures/tree] Implement more functions in the base tree
    Implements:
    - CountLessThan and CountMoreThan
    - FirstGreaterThan and FirstGreaterOrEqualThan (C++ upper_bound and
      lower_bound)
    - At (C++ ordered tree)
    Also fix return values for Max and Min and rename them

--- data-structures/tree/base.go
+++ data-structures/tree/base.go
@@ -14,10 +14,27 @@ type BaseTree[Tk constraints.Ordered, Tv any] struct {
 	val      Tv
 }
 
+type Tree[Tk constraints.Ordered, Tv any] interface {
+	// Read only
+	Find(key Tk) (Tv, error)
+	Min() (Tk, Tv, error)
+	Max() (Tk, Tv, error)
+	IsEmpty() bool
+	Size() int
+	Traverse(func(Tk, Tv))
+	Print()
+	CountLessThan(Tk) int
+	CountMoreThan(Tk) int
+	FirstGreaterThan(Tk) (Tk, Tv, error)
+	FirstGreaterOrEqualThan(Tk) (Tk, Tv, error)
+	At(int) (Tk, Tv, error)
+}
+
 // errors
 var (
-	ErrNotFound = errors.New("Key not found in the tree")
-	ErrEmpty    = errors.New("Tree is empty")
+	ErrNotFound    = errors.New("key not found in the tree")
+	ErrEmpty       = errors.New("tree is empty")
+	ErrOutOfBounds = errors.New("index is out of bounds")
 )
 
 func (t *BaseTree[Tk, Tv]) Find(key Tk) (ret Tv, err error) {
@@ -33,24 +50,24 @@ func (t *BaseTree[Tk, Tv]) Find(key Tk) (ret Tv, err error) {
 	return t.lef.Find(key)
 }
 
-func (t *BaseTree[Tk, Tv]) MinKey() (ret Tv, err error) {
+func (t *BaseTree[Tk, Tv]) Min() (key Tk, val Tv, err error) {
 	if t == nil {
-		return ret, ErrEmpty
+		return key, val, ErrEmpty
 	}
 	if t.lef == nil {
-		return t.val, nil
+		return t.key, t.val, nil
 	}
-	return t.lef.MinKey()
+	return t.lef.Min()
 }
 
-func (t *BaseTree[Tk, Tv]) MaxKey() (ret Tv, err error) {
+func (t *BaseTree[Tk, Tv]) Max() (key Tk, val Tv, err error) {
 	if t == nil {
-		return ret, ErrEmpty
+		return key, val, ErrEmpty
 	}
 	if t.rig == nil {
-		return t.val, nil
+		return t.key, t.val, nil
 	}
-	return t.rig.MaxKey()
+	return t.rig.Max()
 }
 
 func (t *BaseTree[Tk, Tv]) IsEmpty() bool {
@@ -74,6 +91,26 @@ func (t *BaseTree[Tk, Tv]) Traverse(f func(Tk, Tv)) {
 	t.rig.Traverse(f)
 }
 
+func (t *BaseTree[Tk, Tv]) CountLessThan(key Tk) int {
+	if t == nil {
+		return 0
+	}
+	if t.key < key { // go right
+		return t.lef.Size() + 1 + t.rig.CountLessThan(key)
+	}
+	return t.lef.CountLessThan(key)
+}
+
+func (t *BaseTree[Tk, Tv]) CountMoreThan(key Tk) int {
+	if t == nil {
+		return 0
+	}
+	if t.key > key { // go left
+		return t.rig.Size() + 1 + t.lef.CountMoreThan(key)
+	}
+	return t.rig.CountMoreThan(key)
+}
+
 func (t *BaseTree[Tk, Tv]) printImpl() {
 	// Print prints the Tree's key-value pairs using in-order traversal
 	t.Traverse(func(key Tk, val Tv) {
@@ -89,4 +126,40 @@ func (t *BaseTree[Tk, Tv]) Print() {
 	}
 	t.printImpl()
 	fmt.Println()
+}
+
+func (t *BaseTree[Tk, Tv]) firstGreaterThanImpl(key Tk, orEqual bool) (k Tk, v Tv, retErr error) {
+	if t == nil {
+		return k, v, ErrNotFound
+	}
+	shouldGoRight := t.key < key || (!orEqual && t.key == key)
+	if shouldGoRight { // go right
+		return t.rig.firstGreaterThanImpl(key, orEqual)
+	}
+	lk, lv, err := t.lef.firstGreaterThanImpl(key, orEqual)
+	if err != nil { // that means I am the greater
+		return t.key, t.val, nil
+	}
+	return lk, lv, nil
+}
+
+func (t *BaseTree[Tk, Tv]) FirstGreaterThan(key Tk) (Tk, Tv, error) {
+	return t.firstGreaterThanImpl(key, false /*orEqual*/)
+}
+
+func (t *BaseTree[Tk, Tv]) FirstGreaterOrEqualThan(key Tk) (Tk, Tv, error) {
+	return t.firstGreaterThanImpl(key, true /*orEqual*/)
+}
+
+func (t *BaseTree[Tk, Tv]) At(idx int) (k Tk, v Tv, err error) {
+	if t == nil {
+		return k, v, ErrOutOfBounds
+	}
+	sizeLeft := t.lef.Size()
+	if idx == sizeLeft {
+		return t.key, t.val, nil
+	} else if idx > sizeLeft {
+		return t.rig.At(idx - sizeLeft - 1)
+	}
+	return t.lef.At(idx)
 }
